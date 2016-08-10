5479443f852961a3b562f00bc28d111d50fc8098
Ludvig Fischerström <fisken@spotify.com>
2016-08-10 11:41:09 +0200

    home base done and profile picure placeholder added

--- frontend/app/controllers/home_controller.js
+++ frontend/app/controllers/home_controller.js
@@ -1,3 +1,22 @@
 app.controller('HomeController', function($scope) {
   console.log('HomeController');
+
+  $scope.concerts = [
+  	{
+  		date: "2016.08.11",
+  		image_url: "app/assets/M83.jpg",
+  		rating: 3,
+  		venue: {
+  			name: "Way out west"
+  		},
+  		artist: {
+  			name: "M83"
+  		},
+  		user: {
+  			first_name: "Axel",
+  			surname: "Samuelsson" 
+  		}
+  	}
+  ]
+
 });
